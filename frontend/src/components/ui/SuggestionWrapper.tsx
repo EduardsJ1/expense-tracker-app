712188840ec3f60fe7c7175a9613895b51e5888c
Eduards <eduards.janitis@edu.rtu.lv>
2025-06-24 22:03:48 +0300

    fixed the SuggestionWrapper

--- frontend/src/components/ui/SuggestionWrapper.tsx
+++ frontend/src/components/ui/SuggestionWrapper.tsx
@@ -18,7 +18,7 @@ function SuggestionWrapper({ children,suggestionData, onSelect, onClose, display
         <div className="relative" ref={suggestionRef}>
             {children}
             <div className={`mt-1 bg-white absolute z-40 border border-neutral-300 rounded-lg w-full shadow-lg max-h-60 overflow-y-auto ${shouldDisplay?"block":"hidden"}`}>
-                {suggestionData.map((item)=>(
+                {suggestionData.length>0&&suggestionData.map((item)=>(
                     <div className="w-full text-left px-3 py-2 hover:bg-neutral-50 first:rounded-t-lg last:rounded-b-lg cursor-pointer"
                         key={item} onClick={()=>handleSelect(item)}>
                         {item}
