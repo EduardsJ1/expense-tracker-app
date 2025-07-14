2b26d50190d6cfaedd78ea4291923197d945ba73
Eduards <eduards.janitis@edu.rtu.lv>
2025-07-14 19:30:44 +0300

    Icon simple Export

---
+++ frontend/src/components/ui/icons/index.ts
@@ -0,0 +1,13 @@
+export {default as ActivityIcon} from './ActivityIcons';
+export {default as AnalyticsArrow} from './AnalyticsArrow'
+export { default as AnalyticsIcon } from './AnalyticsIcon';
+export {default as DollarIcon} from "./DollarSign";
+export {default as DropDwonIcon} from './DropdownIcon';
+export {default as EditIcon} from "./EditIcon";
+export {default as HamburgerIcon} from "./HamburgerIcon"
+export {default as PausedIcon} from "./PausedIcon";
+export { default as RecurringIcon } from './ReccurringIcon';
+export {default as ResumeIcon} from "./ResumeIcons";
+export { default as TransactionIcon } from './TransactionIcon';
+export {default as TrashIcon} from "./TrashIcon";
+
